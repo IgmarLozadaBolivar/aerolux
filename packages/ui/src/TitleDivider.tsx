2ff343921c97a0b97736a456ac69565aeee6aa69
IgmarLozadaBolivar <losadabolivar@gmail.com>
2025-06-21 09:20:27 -0500

    refactor: 🎨 new structure for the component and add best styling for the view users

--- packages/ui/src/TitleDivider.tsx
+++ packages/ui/src/TitleDivider.tsx
@@ -7,12 +7,11 @@ interface Props {
 
 export const TitleDivider = ({ title, className }: Props) => {
     return (
-        <div className = { className }>
-            <h2 className = "text-5xl font-reaktifNews tracking-wide w-fit pb-2" >
-                { title }
+        <div className={`inline-block ${className ?? ""}`}>
+            <h2 className="text-5xl font-reaktifNews tracking-wide w-fit pb-2">
+                {title}
             </h2>
-
-            <hr className = { "w-[688px] border-t-[3px] border-[#00A896]" } />
+            <hr className={`border-t-[3px] border-[#00A896] w-full ${className ?? ""}`} />
         </div>
     );
 }
